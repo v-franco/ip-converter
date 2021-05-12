cb6271e9d9a64fd5bcd41bf45b87477739b3f23e
Victor Franco <vfranco.itc@gmail.com>
2021-05-12 13:36:33 -0500

    String format changed in IPConverter.py

--- IPConverter.py
+++ IPConverter.py
@@ -7,18 +7,17 @@ from GetValue import getValue
 class IP:
 
     def __init__(self):
-        self.ip = ""
+        self.ip = ''
 
     def readIP(self):
-        self.ip=input("Ingrese la IP: ")
-        print("------------------------")
+        self.ip=input('Ingrese la IP: ')
         return self.ip
         
     def intToBin(self):
-        if self.ip != "":
-            divs = self.ip.split(".")
+        if self.ip != '':
+            divs = self.ip.split('.')
             length = len(divs)
-            self.ip = ""
+            self.ip = ''
             for i in range(0,length):
                 octet = int(divs[i])
                 octet = bin(octet)
@@ -26,41 +25,41 @@ class IP:
                 octet = octet.replace('b','')
                 for j in range(0,6):
                     if (len(octet) < 8):
-                        octet = "0" + octet
-                self.ip = self.ip + octet + "."
+                        octet = '0' + octet
+                self.ip = self.ip + octet + '.'
             self.ip = self.ip[:-1]
             return(self.ip) 
         else:
-            print("Ingrese una IP para convertir con la opción 1)")
+            print('Ingrese una IP para convertir con la opción 1)')
 
     def binToInt(self):
-        if self.ip != "":
-            divs = self.ip.split(".")
+        if self.ip != '':
+            divs = self.ip.split('.')
             length = len(divs)
-            self.ip = ""
+            self.ip = ''
             for i in range(0,length):
                 octet = divs[i]
-                octet = "0b"+octet
+                octet = '0b'+octet
                 octet = int(octet,2)
-                self.ip = self.ip + str(octet) + "."    
+                self.ip = self.ip + str(octet) + '.'    
             self.ip = self.ip[:-1]
             return(self.ip) 
         else:
-            print("Ingrese una IP para convertir con la opción 1)")
+            print('Ingrese una IP para convertir con la opción 1)')
 
     def intToHex(self):
-        if self.ip != "":
-            divs = self.ip.split(".")
+        if self.ip != '':
+            divs = self.ip.split('.')
             length = len(divs)
-            self.ip = ""
+            self.ip = ''
             for i in range(0,length):
                 octet = int(divs[i])
                 octet = hex(octet)[2:]
-                self.ip = self.ip + octet + "."
+                self.ip = self.ip + octet + '.'
             self.ip = self.ip[:-1]
             return(self.ip) 
         else:
-            print("Ingrese una IP para convertir con la opción 1)")
+            print('Ingrese una IP para convertir con la opción 1)')
 
     def binToHex(self):
         self.binToInt()
@@ -68,18 +67,18 @@ class IP:
         return(self.ip)
 
     def hexToInt(self):
-        if self.ip != "":
-            divs = self.ip.split(".")
+        if self.ip != '':
+            divs = self.ip.split('.')
             length = len(divs)
-            self.ip = ""
+            self.ip = ''
             for i in range(0,length):
                 octet = divs[i]
                 octet = int(octet, 16)
-                self.ip = self.ip + str(octet) + "."
+                self.ip = self.ip + str(octet) + '.'
             self.ip = self.ip[:-1]    
             return(self.ip)
         else:
-            print("Ingrese una IP para convertir con la opción 1)")
+            print('Ingrese una IP para convertir con la opción 1)')
     
     def hexToBin(self):
         self.hexToInt()
@@ -93,20 +92,20 @@ def menu():
     option=0
     while option!=10:
 
-        print("------------------------------")
-        print("------------MENU--------------")
-        print("------------------------------")
-        print("1) Leer IP en Dec, Hex, o Bin     ")
-        print("2) Convertir decimal a binario    ")
-        print("3) Convertir binario a decimal    ")
-        print("4) Convertir decimal a hexadecimal")
-        print("5) Convertir binario a hexadecimal")
-        print("6) Convertir hexadecimal a decimal")
-        print("7) Convertir hexadecimal a binario")
-        print("8) Salir                          ")
+        print('------------------------------')
+        print('------------MENU--------------')
+        print('------------------------------')
+        print('1) Leer IP en Dec, Hex, o Bin     ')
+        print('2) Convertir decimal a binario    ')
+        print('3) Convertir binario a decimal    ')
+        print('4) Convertir decimal a hexadecimal')
+        print('5) Convertir binario a hexadecimal')
+        print('6) Convertir hexadecimal a decimal')
+        print('7) Convertir hexadecimal a binario')
+        print('8) Salir                          ')
         
         
-        op=getValue("Indica opción deseada : ",li=1,ls=8,cycle="yes")
+        op=getValue('Indica opción deseada : ',li=1,ls=8,cycle='yes')
         option=op.getNumber()
         if option==1:
             converter.readIP()
